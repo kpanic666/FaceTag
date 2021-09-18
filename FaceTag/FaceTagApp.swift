58d04445032e9421917455cd19cced54cc121275
Andrei Korikov <kpanic666@gmail.com>
2021-09-18 21:29:56 +0500

    Initial Commit

---
+++ FaceTag/FaceTagApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceTagApp.swift
+//  FaceTag
+//
+//  Created by Andrei Korikov on 18.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct FaceTagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
